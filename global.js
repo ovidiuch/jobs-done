ec3a08160a9e890829c093e6317f196eb3bc6543
Ovidiu Cherecheș <skidding@gmail.com>
2018-08-03 13:40:13 +0300

    Remove global styles

--- global.js
+++ global.js
@@ -1,18 +1,3 @@
 // NOTE: react-native-web says "Your application may need to polyfill Promise,
 // Object.assign, Array.from, and ResizeObserver as necessary for your desired
 // browser support."
-
-// TODO: Remove or rename to `global-web`
-import { injectGlobal } from 'styled-components';
-
-injectGlobal`
-  html,
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, Ubuntu, 'Helvetica Neue', Helvetica, sans-serif;
-  }
-
-  button {
-    font-family: -apple-system, BlinkMacSystemFont, Ubuntu, 'Helvetica Neue', Helvetica, sans-serif;
-  }
-
-`;
