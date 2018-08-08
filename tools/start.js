d9e224f162a104a9957fb1a8ef2023eb23a379b1
Ovidiu Cherecheș <skidding@gmail.com>
2018-08-08 14:14:46 +0300

    Pass CLI args to webpack-serve

--- tools/start.js
+++ tools/start.js
@@ -1,11 +1,11 @@
 import serve from 'webpack-serve';
+import { argv } from 'yargs';
 import { getAppWebpackConfig } from './cosmos-webpack-config';
 import { getIndexFile } from './index-file';
 
 start();
 
 async function start() {
-  const argv = {};
   const scriptFilename = 'main.js';
   const config = getAppWebpackConfig({
     path: '/',
@@ -13,7 +13,8 @@ async function start() {
     mode: 'development'
   });
 
-  serve(argv, { config }).then(result => {
+  const { host = 'localhost' } = argv;
+  serve({ host }, { config }).then(result => {
     const { app } = result;
 
     app.use(async ctx => {
