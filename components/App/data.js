fc00a8851c704c2349353cff8972807193d0885b
Ovidiu Cherecheș <skidding@gmail.com>
2018-09-03 14:18:22 +0300

    Update links

--- components/App/data.js
+++ components/App/data.js
@@ -3,20 +3,20 @@ export const steps = [
     name: 'Reply to (or schedule) anything urgent',
     urls: [
       'https://mail.google.com/mail/',
-      'slack://react-cosmos.slack.com/messages/general/'
+      'https://react-cosmos.slack.com/messages/general/'
     ]
   },
   {
     name: 'Transfer mental tasks and notes to official task list',
     urls: [
       'https://keep.google.com/u/0/',
-      'https://workflowy.com/#/4af9601ba880'
+      'https://workflowy.com/#/b63a7ca3e2c3'
     ]
   },
   {
     name: 'Skim over all tasks and check next calendar days',
     urls: [
-      'https://workflowy.com/#/4af9601ba880',
+      'https://workflowy.com/#/b63a7ca3e2c3',
       'https://calendar.google.com/calendar/r'
     ]
   },
