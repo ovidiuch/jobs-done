c89f567610a2338d4dcc41381b84aad734fc3797
Ovidiu Cherecheș <skidding@gmail.com>
2018-10-24 22:48:22 +0300

    Add routing to SSR

---
+++ components/App/Routes.js
@@ -0,0 +1,34 @@
+import { object } from 'prop-types';
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { App } from './App';
+
+export function Routes() {
+  return <Route path="/:stepIndex([0-9]+)?" component={AppRoute} />;
+}
+
+class AppRoute extends Component {
+  static propTypes = {
+    history: object.isRequired,
+    match: object.isRequired
+  };
+
+  render() {
+    const { match } = this.props;
+
+    return (
+      <App
+        activeStepIndex={getActiveStepIndexFromParams(match.params)}
+        setActiveStepIndex={this.setActiveStepIndex}
+      />
+    );
+  }
+
+  setActiveStepIndex = stepIndex => {
+    this.props.history.push(`/${stepIndex}`);
+  };
+}
+
+function getActiveStepIndexFromParams(params) {
+  return typeof params.stepIndex !== 'undefined' ? Number(params.stepIndex) : 0;
+}
