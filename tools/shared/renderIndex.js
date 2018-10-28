d5e0d34a40633dd87016d1bfb1b92fac719bfaf3
Ovidiu Cherecheș <skidding@gmail.com>
2018-10-28 19:04:39 +0200

    Add assets

--- tools/shared/renderIndex.js
+++ tools/shared/renderIndex.js
@@ -24,11 +24,16 @@ export function renderIndex({ scriptFilename }) {
     <title>Jobs Done!</title>
     <meta
       name="description"
-      content="A shutdown ritual app inspired by Deep Work"
+      content="A ritual app for ending the work day"
     />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="theme-color" content="#000915" />
+    <meta name="msapplication-TileColor" content="#603cba">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
     ${style}
   </head>
   <body>
