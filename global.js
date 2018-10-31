1e91a801371262dd9b036c1001796e1b70ba8533
Ovidiu Cherecheș <skidding@gmail.com>
2018-10-31 15:11:32 +0200

    Add ResizeObserver polyfill
    Required by Firefox

--- global.js
+++ global.js
@@ -1,3 +1,8 @@
 // NOTE: react-native-web says "Your application may need to polyfill Promise,
 // Object.assign, Array.from, and ResizeObserver as necessary for your desired
 // browser support."
+import ResizeObserver from 'resize-observer-polyfill';
+
+if (typeof global.ResizeObserver === 'undefined') {
+  global.ResizeObserver = ResizeObserver;
+}
